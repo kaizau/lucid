9441c877ae4825a4382d7017492702b665f43a52
Yifei Zhang <yz@yifei.co>
2011-07-01 13:01:49 -0400

    rm extra line from manifest

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -1,4 +1,4 @@
-description "A Compass/SASS grid for devs who care about semantics."
+description "A Compass/SASS grid for developers who care about semantics."
 
 discover :all
 
@@ -8,8 +8,6 @@ help %Q{
 
 THE LUCID GRID
 plugin by Yifei Zhang [http://yifei.co]
-
-A Compass/SASS grid for developers who love semantics.
 
 A Compass/SASS grid that outputs lean, semantic stylesheets. Avoids CSS repetition on SASS compilation and reduces the need for wrapper <div>s.
 
